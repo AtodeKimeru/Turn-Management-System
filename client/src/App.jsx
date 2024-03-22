5f52007d9b76bc88e60860f03bb6d15872a86c5d
vanCBarE <ibarragan@unal.edu.co>
2024-03-22 13:29:40 -0600

    feat(client): structure created & added components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { UserLogView } from './views/UserLogView';
+import { UserRequestView } from './views/UserRequestView';
+import { AdvisorLogView } from './views/AdvisorLogView';
+import { AdvisorInactiveView } from './views/AdvisorInactiveView';
+import { AdvisorAvaibleView } from './views/AdvisorAvaibleView';
+import { AdvisorInTurnView } from './views/AdvisorInTurnView';
+import { StatsView } from './views/StatsView';
+import { CurrentTurnsView } from './views/CurrentTurnsView';
+import { NotFoundView } from './views/NotFoundView';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/users/log" element={<UserLogView />} />
+          <Route path="/users/request-turn" element={<UserRequestView />} />
+          <Route path="/advisors/log" element={<AdvisorLogView />} />
+          <Route path="/advisors/inactive" element={<AdvisorInactiveView />} />
+          <Route path="/advisors/avaible" element={<AdvisorAvaibleView />} />
+          <Route path="/advisors/in-turn" element={<AdvisorInTurnView />} />
+          <Route path="/stats" element={<StatsView />} />
+          <Route path="/turns" element={<CurrentTurnsView />} />
+          <Route path="*" element={<NotFoundView />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
